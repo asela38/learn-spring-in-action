8e08d67a5feb51528756ff3cba60e0497e06ef79
Anton Asela <to.anton.asela@gmail.com>
2024-01-03 11:57:05 +0800

    add date fields to objects

--- taco-cloud/src/main/java/tacos/Taco.java
+++ taco-cloud/src/main/java/tacos/Taco.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.Date;
 import java.util.List;
 
 @Data
@@ -12,6 +13,8 @@ public class Taco {
     @NotNull
     @Size(min=5, message="Name must be at least 5 characters long")
     private String name;
+
+    private Date createdAt = new Date();
 
     @NotNull
     @Size(min=1, message="You must choose at least 1 ingredient")
--- taco-cloud/src/main/java/tacos/TacoOrder.java
+++ taco-cloud/src/main/java/tacos/TacoOrder.java
@@ -6,11 +6,16 @@ import org.hibernate.validator.constraints.CreditCardNumber;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
+import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Data
-public class TacoOrder {
+public class TacoOrder implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
     @NotBlank(message = "Delivery name is required")
     private String deliveryName;
     @NotBlank(message = "Street is required")
@@ -27,6 +32,7 @@ public class TacoOrder {
     private String ccExpiration;
     @Digits(integer = 3,  fraction = 0, message = "Invalid CVV")
     private String ccCVV;
+    private Date placedAt;
 
     private List<Taco> tacos = new ArrayList<>();
 
