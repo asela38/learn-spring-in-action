f07e96ba4c562214faa2149d495894288984d82d
Anton Asela <to.anton.asela@gmail.com>
2024-01-13 22:28:13 +0800

    add rest endpoints

--- taco-cloud-jpa/src/main/java/tacos/web/api/TacoController.java
+++ taco-cloud-jpa/src/main/java/tacos/web/api/TacoController.java
@@ -2,12 +2,14 @@ package tacos.web.api;
 
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import tacos.data.jdbc.TacoRepository;
 import tacos.model.Taco;
+import tacos.model.TacoOrder;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/api/tacos",
@@ -26,4 +28,18 @@ public class TacoController {
         PageRequest page = PageRequest.of(0, 12, Sort.by("createdAt").descending());
         return tacoRepository.findAll(page).getContent();
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Taco> tacoById(@PathVariable("id") Long id) {
+        return tacoRepository.findById(id)
+                .map(taco -> new ResponseEntity(taco, HttpStatus.OK))
+                .orElse(new ResponseEntity(null, HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping(consumes = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Taco postTaco(@RequestBody Taco taco) {
+        return tacoRepository.save(taco);
+    }
+
 }
